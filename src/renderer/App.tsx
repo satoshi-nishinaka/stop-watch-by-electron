231275b4284f3171437f5d9b6488b98ca5fcab61
Satoshi Nishinaka <satoshie.sp@gmail.com>
2023-02-24 03:48:56 +0900

    ストップウォッチのコードを移植 https://github.com/satoshi-nishinaka/stop-watch/blob/main/src/App.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,65 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
+import { useStopwatch } from 'react-timer-hook';
+import React from 'react';
 
+function ResetButton(props: { clickEvent: () => void }) {
+  return <button onClick={props.clickEvent}>RESET</button>;
+}
+
+function StartPauseButton(props: { running: boolean; clickEvent: () => void }) {
+  if (!props.running) {
+    return <button onClick={props.clickEvent}>START</button>;
+  }
+
+  return <button onClick={props.clickEvent}>PAUSE</button>;
+}
+
+const elapse = (
+  running: boolean,
+  hours: number,
+  minutes: number,
+  seconds: number
+): string => {
+  return `${hours.toString().padStart(2, '0')}:${minutes
+    .toString()
+    .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+};
 function Hello() {
+  const { seconds, minutes, hours, isRunning, start, pause, reset } =
+    useStopwatch({ autoStart: false });
+
   return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+    <div className="App">
+      <header className='App-header'>
+        Stop Watch
+      </header>
+      <section className="main">
+        <div>
+          <div className="timer">
+            {elapse(isRunning, hours, minutes, seconds)}
+          </div>
+        </div>
+        <div className="buttons">
+          <ResetButton
+            clickEvent={() => {
+              // 2回ボタンを押すとちゃんとリセットされるんだけど…
+              reset();
+              pause();
+            }}
+          />
+          <StartPauseButton
+            running={isRunning}
+            clickEvent={() => {
+              if (isRunning) {
+                pause();
+              } else {
+                start();
+              }
+            }}
+          />
+        </div>
+      </section>
     </div>
   );
 }
