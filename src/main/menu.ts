6dd51f005d1cd824d35ef60aa66116acdc50fda2
Satoshi Nishinaka <satoshie.sp@gmail.com>
2023-02-24 04:12:36 +0900

    タイトルの変更

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -54,7 +54,7 @@ export default class MenuBuilder {
 
   buildDarwinTemplate(): MenuItemConstructorOptions[] {
     const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Electron',
+      label: 'Stop Watch',
       submenu: [
         {
           label: 'About ElectronReact',
